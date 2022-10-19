0defc885ade448bf6eedb6046470e15d3c19dfca
sae1013 <sae1013@gmail.com>
2022-10-20 00:25:36 +0900

    다크모드

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import reactLogo from './assets/react.svg'
 import './App.scss'
 
 function App() {
+  const toggleMode = () => {
+    const htmlDom = document.getElementsByTagName('html')[0];
+    htmlDom.classList.toggle("dark")
 
+  }
   return (
     <div className="App">
-
+      <button onClick={toggleMode}>토글</button>
     </div>
   )
 }
 
 export default App
+
