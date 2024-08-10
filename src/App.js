136915fecfc705a7649cc726ecb2c93500171586
MinwooJung <sae1013@gmail.com>
2024-08-10 21:06:47 +0900

    init project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Button from "./design-components/Button";
+import axios from "axios";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Button styles="bg-blue-600">버튼</Button>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+      <Router>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
 const pxToRem = (px) => `${px / 16}rem`;
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
-    spacing: {
-      ...Array.from({ length: 100 }, (_, index) => index + 1).reduce(
-        (acc, px) => {
-          acc[`${px}pxr`] = pxToRem(px);
-          return acc;
-        },
-        {},
-      ),
+    screens: {
+      mobile: "360px",
+      tablet: "768px",
+      desktop: "1024px",
+      "wide-screen": "1740px",
+      "under-desktop": { max: "1023px" },
+    },
+    extend: {
+      spacing: {
+        ...Array.from({ length: 100 }, (_, index) => index + 1).reduce(
+          (acc, px) => {
+            acc[`${px}pxr`] = pxToRem(px);
+            return acc;
+          },
+          {}
+        ),
+      },
     },
   },
   plugins: [],
